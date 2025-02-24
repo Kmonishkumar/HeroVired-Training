8d98aa1ad13601302eb22cde48553b7ee9eeb397
Monish Kumar .K <129575480+Kmonishkumar@users.noreply.github.com>
2025-02-24 15:22:31 +0530

    Update Login.js

--- JavaScript/Loginpage/Login.js
+++ JavaScript/Loginpage/Login.js
@@ -2,11 +2,11 @@ document.getElementById("login-form").addEventListener("submit", function (event
     event.preventDefault(); // Prevent form from refreshing the page
   
     // Get the input values
-    let username = document.getElementById("Uname").value;
+    let username = document.getElementById("uname").value;
     let password = document.getElementById("pwd").value;
   
     // Store the values in local storage
-    localStorage.setItem("Uname", username);
+    localStorage.setItem("uname", username);
     localStorage.setItem("pwd", password);
   
   });
