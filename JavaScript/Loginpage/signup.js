99a7dec21e70c79cbbd90d13c13d3d8b5fb9ca46
Monish Kumar .K <129575480+Kmonishkumar@users.noreply.github.com>
2025-02-24 15:25:37 +0530

    Update signup.js

--- JavaScript/Loginpage/signup.js
+++ JavaScript/Loginpage/signup.js
@@ -2,13 +2,13 @@ document.getElementById("signup-form").addEventListener("submit", function (even
   event.preventDefault(); // Prevent form from refreshing the page
 
   // Get the input values
-  let s_username = document.getElementById("username").value;
-  let s_Email = document.getElementById("email").value;
-  let s_password = document.getElementById("password").value;
+  let s_username = document.getElementById("signup_username").value;
+  let s_Email = document.getElementById("signup_email").value;
+  let s_password = document.getElementById(signup_"password").value;
 
   // Store the values in local storage
-  localStorage.setItem("username", s_username);
-  localStorage.setItem("email", s_Email);
-  localStorage.setItem("password", s_password);
+  localStorage.setItem("signup_username", s_username);
+  localStorage.setItem("signup_email", s_Email);
+  localStorage.setItem("signup_password", s_password);
 
 });
