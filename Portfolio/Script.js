002c5bfacd8bde85197175419ac36a215d9ac3ac
Monish Kumar .K <129575480+Kmonishkumar@users.noreply.github.com>
2025-07-28 14:11:02 +0530

    Update Script.js

--- Portfolio/Script.js
+++ Portfolio/Script.js
@@ -1,36 +1,7 @@
 // Function for the mobile menu toggle
 function toggleMenu() {
-    var menu = document.getElementById("nav-links");
-    if (menu.style.display === "flex") {
-        menu.style.display = "none";
-    } else {
-        menu.style.display = "flex";
-    }
+    document.getElementById("nav-links").classList.toggle("active");
 }
 
-// Form submission validation
-document.getElementById('contactor').addEventListener('submit', function(event) {
-    event.preventDefault();
 
-    var fnameField = document.getElementById('fname');
-    var emailField = document.getElementById('email');
-    var messageField = document.getElementById('msg');
 
-    var fname = fnameField.value;
-    var Ename = emailField.value;
-    var message = messageField.value;
-
-    console.log(fname);
-    console.log(Ename);
-    console.log(message);
-
-    if (!fname || !Ename || !message) {
-        alert("Please Fill all the fields in the Contact Form!!!");
-        return;
-    }
-
-    // Clear the input fields after successful submission
-    fnameField.value = "";
-    emailField.value = "";
-    messageField.value = "";
-});
